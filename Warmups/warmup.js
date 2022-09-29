6f30080f9648073eb8eb0bbe87d9dba9b6e11abc
akyarbrough <ayarbrough0816@gmail.com>
2022-09-29 18:22:13 -0500

    Warmup Exercise 1

---
+++ Warmups/warmup.js
@@ -0,0 +1,9 @@
+let startName = "Amy";
+
+function updateNames(name) {
+    let sentence = `Hello, my name is ${name}. My friend's name is also ${name}.`
+    return sentence;
+}
+console.log(updateNames("Melissa"));
+
+
